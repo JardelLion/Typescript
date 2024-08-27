1334c699307ad1184e95b92ed1d3fee07efdd21c
Jardel Lion <businessjbx@gmail.com>
2024-08-27 17:40:07 +0100

    updated

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let year = 30;
+if (year > 29) {
+    year += 10;
+}
+console.log(year);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let year: number = 30;
+
+if (year > 29){
+    year += 10;
+}
+
+console.log(year);
